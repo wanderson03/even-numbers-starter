d1bdc3e612a681b5116d49010e1a85f2c9ca655d
wanderson03 <wanderson38@student.cccd.edu>
2024-09-20 15:40:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# even-numbers-starter
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/wanderson03/even-numbers-starter)
---
+++ index.js
@@ -0,0 +1,23 @@
+const prompCreator = require('prompt-sync');
+
+const prompt = prompCreator();
+
+// DO NOT CHANGE THE LINE BELOW.
+const n = prompt('Enter a number: ');
+
+console.log(n);
+
+/**
+ * TODO: Create a string of even numbers from 0 to n inclusively,
+ * with a comma as separator between two consecutive
+ * even numbers.
+ * For example: if n = 10, the result is "0,2,4,6,8,10"
+ */
+let result = undefined;
+
+// DO NOT CHANGE THE LINE BELOW.
+console.log(result);
+
+module.exports = {
+  result,
+};
